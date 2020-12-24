11dffd62229eb43ebf41b0d8ee25b5c4d675ecbe
Yashvirani <viraniyash2001@gmail.com>
2020-12-24 12:14:19 +0530

    Updates

---
+++ precache-manifest.9cf2ee86c2b5f5a25b92a3fd2c260593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651f0c4cc9bbf242ee97c715cb086dc3",
+    "url": "/nc/index.html"
+  },
+  {
+    "revision": "6ea64a1cbfb53fc7c127",
+    "url": "/nc/static/css/main.31e57f94.chunk.css"
+  },
+  {
+    "revision": "5fa39151a1c2185636c3",
+    "url": "/nc/static/js/2.296399e5.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/nc/static/js/2.296399e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea64a1cbfb53fc7c127",
+    "url": "/nc/static/js/main.04808b47.chunk.js"
+  },
+  {
+    "revision": "2cf6a25f4e2f6f9d635b",
+    "url": "/nc/static/js/runtime-main.ab79a2ae.js"
+  }
+]);
